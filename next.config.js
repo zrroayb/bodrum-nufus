64864f564974c097c8b1346199a9654c6ef78e22
zrroayb <yamadera1m@gmail.com>
2025-02-07 21:04:35 +0300

    variables for vercel

--- next.config.js
+++ next.config.js
@@ -2,8 +2,17 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ["picsum.photos", "ui-avatars.com"],
+    domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(",") || [
+      "picsum.photos",
+      "ui-avatars.com",
+    ],
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_ENABLE_BLOG: process.env.NEXT_PUBLIC_ENABLE_BLOG,
+    NEXT_PUBLIC_ENABLE_ADMIN: process.env.NEXT_PUBLIC_ENABLE_ADMIN,
   },
 };
 
